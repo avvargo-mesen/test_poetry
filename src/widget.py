2b7d25fea0916482427ec62068272197297f7636
Анастасия Варго <avvargo@gmail.com>
2026-03-25 17:41:41 +0300

    Commit 4 small fixes after insort, mypy and flake in widget.py and masks.py

--- src/widget.py
+++ src/widget.py
@@ -1,52 +1,44 @@
-# def mask_account_card(type_card_account_numbers: str) -> str:
-#     '''Функция возвращает строку с замаскированным номером'''
-#     list_type = []
-#     str_type = ""
-#     list_number = []
-#     str_number =""
-#     splited_string = type_card_account_numbers.split(" ")
-#
-#     #print(splited_string)
-#
-#     for item in splited_string: # смотрим на каждый символ в строке
-#         if item.isalpha():
-#             list_type.append(item) # если это буква, добавляем в лист с названием типа(карта или счет)
-#             str_type = " ".join(list_type)
-#             #print(list_type)
-#             #print(str_type)
-#         if item.isdigit():      # если это число, добавляем в лист с номером
-#             list_number.append(item)
-#             str_number = ''.join(list_number)
-#             #print(str_number)
-#             if len(str_number) == 16: # включаем маскировку для карты
-#                 #print(str_number)
-#                 from masks import get_mask_card_number
-#                 #print(get_mask_card_number(int(str_number)))
-#                 #print(str_type + " " + get_mask_card_number(int(str_number)))
-#                 return str_type + " " + get_mask_card_number(int(str_number))
-#             elif len(str_number) == 20: # включаем маскировку для счета
-#                 #print(str_number)
-#                 from masks import get_mask_account
-#                 #print(get_mask_account(int(str_number)))
-#                 return str_type + " " + get_mask_account(int(str_number))
-#             else:
-#                 return "Введите номер заново"
-#
-# result = mask_account_card("Счет 73654108430135874455")
-# print(result)
+def mask_account_card(type_card_account_numbers: str) -> str:
+    '''Функция возвращает строку с замаскированным номером'''
+    list_type = []
+    str_type = ""
+    list_number = []
+    str_number = ""
+    splited_string = type_card_account_numbers.split(" ")
+
+    for item in splited_string:
+        if item.isalpha():
+            list_type.append(item)
+            str_type = " ".join(list_type)
+        if item.isdigit():
+            list_number.append(item)
+            str_number = ''.join(list_number)
+            if len(str_number) == 16:
+                from src.masks import get_mask_card_number
+
+                return str_type + " " + get_mask_card_number(int(str_number))
+            elif len(str_number) == 20:
+                from src.masks import get_mask_account
+
+                return str_type + " " + get_mask_account(int(str_number))
+            else:
+
+                return "Введите номер заново"
+
+    return "Введите номер заново"  # добавила, т.к. ругался mypy
+
+
+result = mask_account_card("Счет 73654108430135874305")
+print(result)
 
 
 def get_date(current_date: str) -> str:
-#'''Функиця возвращает дату в нужном формате'''
+    '''Функиця возвращает дату в нужном формате'''
     slice_date = current_date[:10]
-    year = slice_date[:4]
-    month = slice_date[5:7]
-    day = slice_date[-2:]
-    new_date_format = day + "." + month + "." + year
-    print(slice_date)
-    print(year)
-    print(month)
-    print(day)
+    new_date_format = f"{slice_date[-2:]}.{slice_date[5:7]}.{slice_date[:4]}"
+
     return new_date_format
+
+
 result = get_date("1999-10-11T02:26:18.671407")
 print(result)
