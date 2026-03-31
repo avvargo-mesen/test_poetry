633b5db7507698bf911672eeeebd38e3a34f8a81
Анастасия Варго <avvargo@gmail.com>
2026-03-31 17:35:32 +0300

    add first function filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def filter_by_state (dict_list: list, state='EXECUTED')->list:
+    """Функция возвращает список словарей, у которых ключ state соответсвует указанному значению"""
+    new_list = []
+    for dict in dict_list:
+        if dict.get("state") == state:
+            new_list.append(dict)
+
+    return new_list
+
+
+# result = filter_by_state ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], state='EXECUTED')
+# print(result)
+
