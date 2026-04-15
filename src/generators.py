bd2a8d89c804bc53ec0ef0bf2886217942059778
Анастасия Варго <avvargo@gmail.com>
2026-04-15 23:17:59 +0300

    Add transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -59,3 +59,61 @@ gen_currency = filter_by_currency([{
 # usd_transactions = filter_by_currency(transactions, "USD")
 # for _ in range(2):
 #     print(next(usd_transactions))
+
+def transaction_descriptions(transactions: list[dict]) -> Generator:
+    '''Функция возвращает описание каждой операции по очереди'''
+    for d in transactions:
+        yield d["description"]
+
+gen_descriptions = transaction_descriptions([{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "RUB",
+                      "code": "RUB"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"},
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2019-08-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9823.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"}])
+
+first_item = next(gen_descriptions)
+print(first_item)
+second_item = next(gen_descriptions)
+print(second_item)
+third_item = next(gen_descriptions)
+print(third_item)
+
+
