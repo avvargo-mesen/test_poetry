419e5619e391712976dc24d9d00fbdaa6a5df764
Анастасия Варго <avvargo@gmail.com>
2026-04-23 13:24:03 +0300

    Add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+def log(filename=None):
+    """Задает файл/консоль для логов"""
+    def my_decorator(func):
+        """Принимает декорируемую функцию"""
+        def wrapper(*args, **kwargs):
+            """Принимает аргументы декорируемой функции и возвращает результат"""
+            start_message = f"Function {func.__name__} started"
+            end_message = f"Function {func.__name__} finished"
+            result = None
+            try:
+                result = func(*args, **kwargs)
+                message = (f'{func.__name__} ok')
+            except Exception as e:
+                message = (f'{func.__name__} error: {e}. Inputs: {args}, {kwargs}')
+            if filename is None:
+                print(start_message)
+                print(message)
+                print(end_message)
+            else:
+                with open(filename, 'a') as file:
+                    file.write(start_message + '\n')
+                    file.write(message + '\n')
+                    file.write(end_message + '\n')
+
+
+            return result
+
+        return wrapper
+
+    return my_decorator
