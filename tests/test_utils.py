c1b1a1251b509805eaf16a3eb4ff49acac75527a
Анастасия Варго <avvargo@gmail.com>
2026-05-04 21:23:17 +0300

    Add tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,60 @@
 import pytest
 from src.utils import read_json_file
+from unittest.mock import Mock, patch
+
+# def test_read_json_empty_file():
+#     """тест на пустой файл"""
+#     assert read_json_file("tests/test_empty.json") == []
+#
+#
+# def test_read_json_no_file():
+#     """тест нет переданного в аргументы файла"""
+#     assert read_json_file("non_existent_file.json") == []
+#
+# def test_read_json_file_dict():
+#     """тест не содержит список"""
+#     assert read_json_file("tests/test_not_list.json") == []
+#
+#
+# def test_read_json_good_file():
+#     """тест есть переданный в аргументы файла"""
+#     assert read_json_file("tests/test_operations.json") == [
+#         {
+#             "id": 441945886,
+#             "state": "EXECUTED",
+#             "date": "2019-08-26T10:50:58.294041",
+#             "operationAmount": {
+#                 "amount": "31957.58",
+#                 "currency": {
+#                     "name": "руб.",
+#                     "code": "RUB"
+#                 }
+#             },
+#             "description": "Перевод организации",
+#             "from": "Maestro 1596837868705199",
+#             "to": "Счет 64686473678894779589"
+#         }]
 
 
-def test_read_json_empty_file():
-    """тест на пустой файл"""
-    assert read_json_file("tests/test_empty.json") == []
+@patch('json.load')
+def test_read_json_empty_file(mock_empty):
+    mock_empty.return_value = []
+    assert read_json_file("any_path") == []
 
+@patch('builtins.open')
+def test_read_json_no_file(mock_no_file):
+    mock_no_file.side_effect = FileNotFoundError
+    assert read_json_file("any_path") == []
 
-def test_read_json_file_dict():
-    """тест не содержит список"""
-    assert read_json_file("tests/test_not_list.json") == []
+@patch('json.load')
+def test_read_json_file_dict(mock_dict):
+    mock_dict.return_value = {}
+    assert read_json_file("any_path") == []
 
-
-def test_read_json_no_file():
-    """тест нет переданного в аргументы файла"""
-    assert read_json_file("non_existent_file.json") == []
-
-
-def test_read_json_good_file():
-    """тест есть переданный в аргументы файла"""
-    assert read_json_file("tests/test_operations.json") == [
+@patch('builtins.open')
+@patch('json.load')
+def test_read_json_good_file(mock_empty, mock_open):
+    mock_empty.return_value = [
         {
             "id": 441945886,
             "state": "EXECUTED",
@@ -35,3 +70,20 @@ def test_read_json_good_file():
             "from": "Maestro 1596837868705199",
             "to": "Счет 64686473678894779589"
         }]
+    assert read_json_file("any_path") == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }]
+
