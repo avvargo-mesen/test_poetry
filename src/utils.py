0d5f3996368cfcff4fdc0badfa8f1a85487182f5
Анастасия Варго <avvargo@gmail.com>
2026-05-11 22:55:41 +0300

    Add logs in utils

--- src/utils.py
+++ src/utils.py
@@ -4,37 +4,42 @@ import logging
 from json import JSONDecodeError
 
 logger_utils = logging.getLogger(__name__)
-file_handler = logging.FileHandler('utils.log')
-file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+file_handler = logging.FileHandler('logs/utils.log', encoding='utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger_utils.addHandler(file_handler)
 logger_utils.setLevel(logging.DEBUG)
 
-logger_utils.debug('Debug message')
-logger_utils.info('Info message')
-logger_utils.warning('Warning message')
-logger_utils.error('Error message')
-logger_utils.critical('Critical message')
+# logger_utils.debug('Debug message')
+# logger_utils.info('Info message')
+# logger_utils.warning('Warning message')
+# logger_utils.error('Error message')
+# logger_utils.critical('Critical message')
 
 
 def read_json_file(filename: str) -> list[dict]:
     """принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
     try:
+        logger_utils.info(f'Файл для записи открыт')
         with open(filename, encoding='utf-8') as f:
             data = json.load(f)
 
         if type(data) is list:
+            logger_utils.info(f'Список словарей успешно создан')
             return data
 
         else:
+            logger_utils.error(f'Тип данных не список')
             return []
 
     except FileNotFoundError:
         # Файл не найден
+        logger_utils.error(f'Файл с данными не найден')
         return []
 
     except JSONDecodeError:
         # Файл пустой
+        logger_utils.error(f'Файл с данными пуст')
         return []
 
     # except Exception:
