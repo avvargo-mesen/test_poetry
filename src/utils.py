fa3653212c0e6d02f6afc10097592d8e0afca0bd
Анастасия Варго <avvargo@gmail.com>
2026-05-11 22:46:23 +0300

    Add logs for masks

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,20 @@
 import json
+import logging
+
 from json import JSONDecodeError
+
+logger_utils = logging.getLogger(__name__)
+file_handler = logging.FileHandler('utils.log')
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger_utils.addHandler(file_handler)
+logger_utils.setLevel(logging.DEBUG)
+
+logger_utils.debug('Debug message')
+logger_utils.info('Info message')
+logger_utils.warning('Warning message')
+logger_utils.error('Error message')
+logger_utils.critical('Critical message')
 
 
 def read_json_file(filename: str) -> list[dict]:
